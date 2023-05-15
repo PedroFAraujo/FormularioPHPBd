e93b4a7e667964870632f1179c484f50a7710b1a
PedroFAraujo <pedrofeearaujo@gmail.com>
2023-05-15 06:06:57 -0300

    Conexão PHP com MySQL 15/05/2023

--- index.php
+++ index.php
@@ -20,23 +20,125 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styleDados.css">
         <title>PHP - Sistema de Acesso ao BD</title>
     </head>
     <body>
-        <?php
-            $sql = "SELECT * FROM clientes";
-            $result = $PDO->query($sql);
-            $rows = $result->fetchAll();
-            
-            for($i=0; $i < count($rows); $i++){ ?>
-                Nome: <?php echo $rows[$i]['nome']; ?><br>
-                Endereço: <?php echo $rows[$i]['endereco']; ?><br>
-                Bairro: <?php echo $rows[$i]['bairro']; ?><br>
-                CEP: <?php echo $rows[$i]['cep']; ?><br>
-                Cidade: <?php echo $rows[$i]['cidade']; ?><br>
-                Estado: <?php echo $rows[$i]['estado']; ?><br>
-            <?php 
-                }
-        ?>
+        <div class="container" id="container">
+                <div class="row">
+                    <div class="col">
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary, fundo">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="index.php">PHP - Sistema de Acesso ao Banco de Dados</a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col titles">
+                        <h1>Clientes</h1>
+                    </div>
+                </div>
+            <div class="row">
+                <div class="col boxDados">
+                    <div class="table-responsive">
+                        <table class="table table-borderless" id="tabela">
+                            <thead>
+                                <tr class="headerTable">
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Endereço</th>
+                                    <th scope="col">Bairro</th>
+                                    <th scope="col">Cidade</th>
+                                    <th scope="col">Estado</th>
+                                    <th scope="col">CEP</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['id'];
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['nome'];
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['endereco'];
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['bairro'];
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['cidade']; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['estado']; 
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php
+                                            $sql = "SELECT * FROM clientes";
+                                            $result = $PDO->query($sql);
+                                            $rows = $result->fetchAll();
+                                            
+                                            echo $rows[0]['cep'];
+                                        ?>
+                                    </td>
+                                    <td>
+                                        
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p></p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
